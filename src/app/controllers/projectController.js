06b98d3fc2f2b2586d9a618da973593368c017a9
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-12 09:04:08 -0300

    Finalized Create and Update method

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -3,7 +3,7 @@ const express = require("express");
 const authMiddleware = require("../middlewares/auth");
 
 const Project = require("../models/project");
-const Tast = require("../models/task");
+const Task = require("../models/task");
 
 const router = express.Router();
 
@@ -12,7 +12,9 @@ router.use(authMiddleware);
 
 router.get("/", async (req, res) => {
   try {
-    const projects = await Project.find().populate("user"); // populate() serve para trazer os dados do relacionamento (eager loading). Nesse caso, faz duas consultas, uma para trazer os projetos e outra para trazer o relacionamento.
+    /* populate("someAttr") serve para trazer os dados do  relacionamento (eager loading).
+     * Nesse caso, faz duas consultas, uma para trazer os projetos e outra para trazer o relacionamento. */
+    const projects = await Project.find().populate(["user", "tasks"]);
     return res.send({ projects });
   } catch (error) {
     return res.status(400).send({ Error: "Error loading projects" });
@@ -21,9 +23,10 @@ router.get("/", async (req, res) => {
 
 router.get("/:projectId", async (req, res) => {
   try {
-    const project = await Project.findById(req.params.projectId).populate(
-      "user"
-    );
+    const project = await Project.findById(req.params.projectId).populate([
+      "user",
+      "tasks"
+    ]);
     return res.send({ project });
   } catch (error) {
     return res.status(400).send({ Error: "Error loading project" });
@@ -32,7 +35,37 @@ router.get("/:projectId", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const project = await Project.create({ ...req.body, user: req.userId });
+    const { title, description, tasks } = req.body;
+
+    const project = await Project.create({
+      title,
+      description,
+      user: req.userId
+    });
+
+    // Salvando as tasks do projeto (relacionamento = 1 projeto tem muitas tasks)
+    /* Aguardar todas as tasks serem adicionadas para depois salvá-las no projeto
+     * pois senão ele executa o project.save() antes de concluir o callback
+     */
+    await Promise.all(
+      tasks.map(async task => {
+        // Cria uma task definindo o id do projeto em que ela está vinculada.
+        const projectTask = new Task({
+          ...task,
+          assignedTo: project.user._id,
+          project: project._id
+        });
+
+        // Aguarda até que seja salva a task
+        await projectTask.save();
+
+        // Adiciona a task salva em seu projeto com mesmo id
+        project.tasks.push(projectTask);
+      })
+    );
+
+    // Aguarda todas as tasks serem salvas e adicionadas no projeto e enão salva o projeto
+    await project.save();
 
     return res.send({ project });
   } catch (err) {
@@ -42,7 +75,38 @@ router.post("/", async (req, res) => {
 
 router.put("/:projectId", async (req, res) => {
   try {
-    res.send({ user: req.userId });
+    const { title, description, tasks } = req.body;
+
+    const project = await Project.findByIdAndUpdate(
+      req.params.projectId,
+      {
+        title,
+        description
+      },
+      { new: true }
+    ); // "new true" retorna o projeto atualizado do projeto e não o antigo
+
+    // Deletando todas as tasks pois serão recriadas novamente
+    project.tasks = [];
+    await Task.remove({ project: project._id });
+
+    await Promise.all(
+      tasks.map(async task => {
+        const projectTask = new Task({
+          ...task,
+          assignedTo: project.user._id,
+          project: project._id
+        });
+
+        await projectTask.save();
+
+        project.tasks.push(projectTask);
+      })
+    );
+
+    await project.save();
+
+    return res.send({ project });
   } catch (err) {
     return res.status(400).send({ Error: "Error updating new projetct" });
   }
